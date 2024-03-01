f9f7837999d000456bb19c5575a4ada8fb7b2df0
SSmits2404 <swg.smits@student.avans.nl>
2024-03-01 12:21:43 +0100

    added tables

---
+++ Bazaar/database/migrations/2024_03_01_104421_advert_comments.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('AdvertComments', function(Blueprint $table){
+            $table->id();
+            $table->unsignedBigInteger('reviewer');
+            $table->unsignedBigInteger('advert');
+            $table->integer('review');
+            
+            $table->foreign('reviewer')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('advert')->references('id')->on('adverts')->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('AdvertComments');
+    }
+};
+
