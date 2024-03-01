0e3235becef2ef66cdeeb943039a352729c10765
SSmits2404 <swg.smits@student.avans.nl>
2024-03-01 13:36:40 +0100

    quick add table

---
+++ Bazaar/database/migrations/2024_03_01_122640_rental.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('Rental', function(Blueprint $table){
+            $table->id();
+            $table->unsignedBigInteger('User');
+            $table->unsignedBigInteger('advert');
+            $table->date('startDate');
+            $table->date('EndDate');
+            
+            $table->foreign('user')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('advert')->references('id')->on('adverts')->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('Favorites');
+    }
+};
