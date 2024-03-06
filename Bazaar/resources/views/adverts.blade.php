0efc5ea191dc4a2f80aee24a65713ab7a0ca389f
SSmits2404 <swg.smits@student.avans.nl>
2024-03-06 14:04:46 +0100

    k

--- Bazaar/resources/views/adverts.blade.php
+++ Bazaar/resources/views/adverts.blade.php
@@ -23,6 +23,19 @@
                                 <p>{{__('price')}}: {{ $advert->price }}</p>
                                 <p>{{__('Posted by')}}: {{ $advert->user->name }}</p>
 
+                                @if($advert->user_id == auth()->user()->id)
+                                <button class ='inline-flex items-center px-4 py-2 bg-red-500 dark:bg-red-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-white uppercase tracking-widest hover:bg-red-700 dark:hover:bg-red focus:bg-red-700 dark:focus:bg-red active:bg-red-900 dark:active:bg-red-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-red-800 transition ease-in-out duration-150'>
+                                Delete
+                                </button>
+
+                                @endif
+                                @if($advert->user_id != auth()->user()->id)
+                                
+                                <button class ='inline-flex items-center px-4 py-2 bg-red-500 dark:bg-red-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-white uppercase tracking-widest hover:bg-red-700 dark:hover:bg-red focus:bg-red-700 dark:focus:bg-red active:bg-red-900 dark:active:bg-red-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-red-800 transition ease-in-out duration-150'>
+                                favorite
+                                </button>
+                                
+                                @endif
                             </a>
                         </div>
                     </div>
