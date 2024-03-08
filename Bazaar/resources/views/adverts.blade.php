c20685ab3f2ed37abc9521bd0c5e228537118f3f
SSmits2404 <swg.smits@student.avans.nl>
2024-03-08 10:09:41 +0100

    k

--- Bazaar/resources/views/adverts.blade.php
+++ Bazaar/resources/views/adverts.blade.php
@@ -22,21 +22,22 @@
 
                                 <p>{{__('price')}}: {{ $advert->price }}</p>
                                 <p>{{__('Posted by')}}: {{ $advert->user->name }}</p>
+                            </a>
 
                                 @if($advert->user_id == auth()->user()->id)
-                                <button class ='inline-flex items-center px-4 py-2 bg-red-500 dark:bg-red-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-white uppercase tracking-widest hover:bg-red-700 dark:hover:bg-red focus:bg-red-700 dark:focus:bg-red active:bg-red-900 dark:active:bg-red-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-red-800 transition ease-in-out duration-150'>
+                                <a class ='inline-flex items-center px-4 py-2 bg-red-500 dark:bg-red-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-white uppercase tracking-widest hover:bg-red-700 dark:hover:bg-red focus:bg-red-700 dark:focus:bg-red active:bg-red-900 dark:active:bg-red-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-red-800 transition ease-in-out duration-150'>
                                 Delete
-                                </button>
+                                </a>
 
                                 @endif
                                 @if($advert->user_id != auth()->user()->id)
                                 
-                                <button class ='inline-flex items-center px-4 py-2 bg-red-500 dark:bg-red-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-white uppercase tracking-widest hover:bg-red-700 dark:hover:bg-red focus:bg-red-700 dark:focus:bg-red active:bg-red-900 dark:active:bg-red-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-red-800 transition ease-in-out duration-150'>
+                                <a class ='inline-flex items-center px-4 py-2 bg-red-500 dark:bg-red-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-white uppercase tracking-widest hover:bg-red-700 dark:hover:bg-red focus:bg-red-700 dark:focus:bg-red active:bg-red-900 dark:active:bg-red-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-red-800 transition ease-in-out duration-150'>
                                 favorite
-                                </button>
+                                </a>
                                 
                                 @endif
-                            </a>
+                            
                         </div>
                     </div>
                 @endforeach
