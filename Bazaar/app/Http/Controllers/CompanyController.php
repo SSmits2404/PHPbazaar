df9456a27f8b5934f3146e1df9c65bd908d81b5e
ThomasWesterlaken <82945745+ThomasWesterlaken@users.noreply.github.com>
2024-03-10 14:46:57 +0100

    added companies

--- Bazaar/app/Http/Controllers/CompanyController.php
+++ Bazaar/app/Http/Controllers/CompanyController.php
@@ -3,11 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Company;
 
 class CompanyController extends Controller
 {
     public function index(string $company)
     {
+        $company = Company::where('custom_url', $company)->first();
+        if($company == null)
+        {
+            return view('welcome');
+        }
+        else{
         return view('company', ['company' => $company]);
+        }
     }
+
+
 }
