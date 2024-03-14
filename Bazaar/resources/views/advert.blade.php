abbe8fac3720b15580ed69c004ad8957f7ca6442
SSmits2404 <swg.smits@student.avans.nl>
2024-03-14 17:30:01 +0100

    zooi

--- Bazaar/resources/views/advert.blade.php
+++ Bazaar/resources/views/advert.blade.php
@@ -35,27 +35,23 @@
                                 </a>
 
                                 @endif
-                                /fix
+                                
                                 @if($advert->user_id != auth()->user()->id)
                                 
                                 @php
-                                    $isFavorite = auth()->user()->favorites->contains($advert->id);
+                                   // $isFavorite = auth()->user()->favorites->contains($advert->id);
                                 @endphp
 
                                 
                                 
 
-                                @if ($isFavorite)
+                               
                                     <a href="{{ route('adverts.unfavorite', $advert->id) }}" class="inline-flex items-center px-4 py-2 bg-red-500 dark:bg-red-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-white uppercase tracking-widest hover:bg-red-700 dark:hover:bg-red focus:bg-red-700 dark:focus:bg-red active:bg-red-900 dark:active:bg-red-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-red-800 transition ease-in-out duration-150">
                                     unfavorite
                                 </a>
-                                @else
-                                    <a href="{{ route('adverts.favorite', $advert->id) }}" class="inline-flex items-center px-4 py-2 bg-red-500 dark:bg-red-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-white uppercase tracking-widest hover:bg-red-700 dark:hover:bg-red focus:bg-red-700 dark:focus:bg-red active:bg-red-900 dark:active:bg-red-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-red-800 transition ease-in-out duration-150">
-                                    favorite
-                                    </a>
                                 @endif
 
-                                @endif
+                            
                             </div>
                                 
             </div>
--- Bazaar/resources/views/company.blade.php
+++ Bazaar/resources/views/company.blade.php
@@ -1,4 +1,3 @@
-<div>
     <x-app-layout>
         <x-slot name="header">
             <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
@@ -8,4 +7,3 @@
         </x-slot>
         <div> {{$company}}</div>
     </x-app-layout>
-</div>
