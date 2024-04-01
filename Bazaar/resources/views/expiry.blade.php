9003e9a5cf58fa8f7e7b83766df8095673b4d0a3
SSmits2404 <swg.smits@student.avans.nl>
2024-04-01 15:44:07 +0200

    quick expiry fix

--- Bazaar/resources/views/expiry.blade.php
+++ Bazaar/resources/views/expiry.blade.php
@@ -50,7 +50,7 @@
                                     {{__('Type')}}: {{ $expiry->advert_type}}
                                     </div>
                                     <br>
-                                    @if($expiry->durability == 0 && $expiry->durability != null)
+                                    @if($expiry->durability === 0)
                                     <a href="{{ route('expiry.repair', $expiry->id) }}" class="inline-flex items-center px-4 py-2 bg-red-500 dark:bg-red-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-white uppercase tracking-widest hover:bg-red-700 dark:hover:bg-red focus:bg-red-700 dark:focus:bg-red active:bg-red-900 dark:active:bg-red-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-red-800 transition ease-in-out duration-150">
                                         {{__('repair')}}
                                     </a>
